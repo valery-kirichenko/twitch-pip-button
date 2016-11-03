d0e9732efb74533031092784bcd21aaf64f3e355
Valery Kirichenko <valera5505@gmail.com>
2016-11-03 23:26:37 +0300

    Remove interval

--- twitch-pip-button.user.js
+++ twitch-pip-button.user.js
@@ -13,24 +13,22 @@
 var playerReady = setInterval(function() {
 	if (document.querySelector("div.player-buttons-right")) {
 		clearInterval(playerReady);
-		setTimeout(function() {
-			var css = document.createElement("link");
-			css.rel = "stylesheet";
-			css.href = "https://cdn.rawgit.com/valera5505/twitch-pip-button/master/style.css";
-			document.getElementsByTagName("head")[0].appendChild(css);
+		var css = document.createElement("link");
+		css.rel = "stylesheet";
+		css.href = "https://cdn.rawgit.com/valera5505/twitch-pip-button/master/style.css";
+		document.getElementsByTagName("head")[0].appendChild(css);
 
-			var btn = document.createElement("button");
-			btn.className = "player-button";
-			btn.type = "button";
-			btn.style = "display: inline-block";
-			btn.id = "pip-button";
-			btn.innerHTML = '<span class="icon-1"></span>';
-			btn.style.padding = "1px 5px 1em";
-			
-			document.querySelector("div.player-buttons-right").insertBefore(btn, document.querySelector("button.player-button--fullscreen"));
-			btn.addEventListener("click", function() {
-				document.querySelector("video").webkitSetPresentationMode('picture-in-picture');
-			});
-		}, 100);
+		var btn = document.createElement("button");
+		btn.className = "player-button";
+		btn.type = "button";
+		btn.style = "display: inline-block";
+		btn.id = "pip-button";
+		btn.innerHTML = '<span class="icon-1"></span>';
+		btn.style.padding = "1px 5px 1em";
+
+		document.querySelector("div.player-buttons-right").insertBefore(btn, document.querySelector("button.player-button--fullscreen"));
+		btn.addEventListener("click", function() {
+			document.querySelector("video").webkitSetPresentationMode('picture-in-picture');
+		});
 	}
 }, 100);
